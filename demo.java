9747e95d1c262978316f33fd032e23a35f711fa6
sahil dahake <sahildahake1999@gmail.com>
2022-09-28 12:46:26 +0530

    java

---
+++ demo.java
@@ -0,0 +1,12 @@
+public class demo{
+    public static void main(String args[])
+    {
+        System.out.println("the sum is:");
+        int num1 =6;
+        int num2 =7;
+        int num3 =8;
+        int sum=num1+num2+num3;
+
+        System.out.println(sum);
+    }
+}
