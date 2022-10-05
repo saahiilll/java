5bb1efd8e158c9037370cb4a5f10d8ae73f81606
sahil dahake <sahildahake1999@gmail.com>
2022-10-05 19:37:09 +0530

    commit

---
+++ literals.java
@@ -0,0 +1,18 @@
+public class literals{
+    public static void main (String[] args){
+        byte bt = 34;
+        int age = 36;
+        char ch = 'A';
+        short sh = 3;
+        long ln = 343535l;
+        float fl = 5.6f;
+        double dl = 2.333d;
+        boolean bl = true;
+        String str = "sahil";
+
+
+        System.out.println(age);
+        System.out.println(str);
+    }
+
+}
