e13beb9ba4f950250c4181988b57a6f64491b63f
sahil dahake <sahildahake1999@gmail.com>
2023-02-11 17:58:37 +0530

    commit

---
+++ sahil.java
@@ -0,0 +1,5 @@
+public class sahil{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String[] args) {
+        System.out.println("the sum of this three number is");
+        int num1=3;
+        int num2=4;
+        int num3=5;
+        int sum=num1+num2+num3;
+        System.out.println(sum);
+    }
+}
